f742c28075d3a6f9ef0d50a17bae2fb75df2ee00
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-07 17:05:14 -0500

    adjust asso last time

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
 
   has_many :user_games
   # validates :user_games, :length => { :maximum => 10 }
-  has_many :flags
+  has_many :flags, through: :players
   has_many :users, through: :user_games
 
   def game_expired?
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
                                        :sessions => "sessions" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  #root to: "users#index"
-  resource :users do
-    resource :games
+  root to: "users#index"
+  resources :users do
+    resources :games
   end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
