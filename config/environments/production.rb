f5c78a398ddfb839f60ede02cd764b186e7ac658
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-08 08:02:31 -0400

    add config for Mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,20 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_mailer.default_url_options = {
+    host: 'safe-sands-7813.herokuapp.com'
+  }
+
+  config.action_mailer.smtp_settings = {
+    :port => 587,
+    :address => "smtp.mandrillapp.com",
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password => ENV["MANDRILL_APIKEY"],
+    :domain => "heroku.com",
+    :authentication => :plain
+  }
+  config.action_mailer.delivery_method = :smtp
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
